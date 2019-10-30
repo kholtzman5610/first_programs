e0f660d422f05cdc4db8ef4ca7906e4a42284faa
kholtzman5610 <kassidy.holtzman@outlook.com>
2019-10-30 15:44:55 +0000

    full name

---
+++ full_name.rb
@@ -0,0 +1,14 @@
+puts "Enter first name"
+a = []
+user_input = gets.chomp
+a << user_input
+
+puts "Enter middle name"
+user_input = gets.chomp
+a << user_input
+
+puts "Enter last name"
+user_input = gets.chomp
+a << user_input
+
+puts "Hello #{a}"
