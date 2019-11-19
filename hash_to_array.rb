e1064d6d1bd2554e0a97017a22aa08899449b3d7
kholtzman5610 <kassidy.holtzman@outlook.com>
2019-11-19 21:46:42 +0000

    Hash to Array

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -3,8 +3,6 @@ Write a program that collects 5 keys and 5 values from the user, and stores them
 in a hash. Write a function that accepts the hash as optional parameters and 
 prints out an array of keys and an array of values. Call the function within your
 programs so you know it works.
-
-info #height: '5 ft', weight: '165 lbs', hair: 'brown', : 'hazel'}  
 =end
 
 
