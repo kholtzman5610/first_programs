3463ca228c880da6497a514aa4425a39218bc231
kholtzman5610 <kassidy.holtzman@outlook.com>
2019-11-19 21:15:21 +0000

    Hash to Array

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -3,11 +3,21 @@ Write a program that collects 5 keys and 5 values from the user, and stores them
 in a hash. Write a function that accepts the hash as optional parameters and 
 prints out an array of keys and an array of values. Call the function within your
 programs so you know it works.
+
+info #height: '5 ft', weight: '165 lbs', hair: 'brown', : 'hazel'}  
 =end
 
-person = {name: 'bob', height: '5 ft', weight: '165 lbs', hair: 'brown', 
-eye: 'hazel'}
 
-person.each do |key, value|
-    puts "Bob's #{key} is #{value}"
+def person(name, options = {})
+  if options.empty?
+    puts "Hi, my name is #{name}"
+  else
+    puts "Hi, my name is #{name} and I'm #{options[:age]}" + " years old." +
+    " I'm #{options[:height]} tall." + " I have #{options[:hair]} hair," +
+    " and #{options[:eye]} eyes. I live in #{options[:city]}." 
+  end
 end
+
+person("Bob")
+person("Bob", {age: 45, height: '5 ft', hair: 'brown', eye: 'hazel', 
+city: 'New York City'})
